2569c00f9c7887cce573b0f2cdde83295be1f9b2
Karpenko Nataliia <Netaly79@gmail.com>
2019-09-27 22:39:20 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+
+"use strict";
+const  url="https://trevadim.github.io/vue/data/data.json";
+const body = document.querySelector('body');
+
+let createHeader = (text) => {
+    let header = document.createElement("header");
+    header.innerHTML=`<h1>${text}</h1>`;
+    header.style.textAlign="center";
+    body.appendChild(header);
+}
+let createMarkUp= ({main},{planets}) => {
+    createHeader(main.cosmos);
+    createMain({head:main.infoCosmos, paragraf:main.infoFly});
+    createPlanets({planets});
+    
+}
+
+let createMain=({head,paragraf}) =>{
+    let main=document.createElement("main");
+    main.innerHTML=`<h2>${head}</h2><p>${paragraf}</p>`;
+    main.style.textAlign="center";
+    body.appendChild(main);
+}
+let createPlanets=({planets}) => {
+
+    let mainList=document.createElement("ul");
+    mainList.innerText="Планеты солнечной системы";
+    mainList.style.textAlign="center";
+    mainList.style.fontSize="26px";
+    mainList.style.fontWeight="bold";
+    mainList.style.paddingBottom="20px";
+    body.appendChild(mainList);
+
+    for (let i in planets){
+    let planet=createPlanet(planets[i]);
+    mainList.appendChild(planet);
+    }
+}
+
+let createPlanet=(planet) => {
+    let newPlanet=document.createElement("li");
+    newPlanet.innerHTML=`${planet.title} <br>`;
+    newPlanet.style.textAlign="center";
+    newPlanet.style.fontSize="24px";
+    newPlanet.style.marginTop="24px";
+    let imagePlanet=document.createElement("img");
+    imagePlanet.setAttribute('src',`${planet.url}`);
+    imagePlanet.style.display="block";
+    imagePlanet.style.margin="0 auto";
+    newPlanet.appendChild(imagePlanet);
+    for (let i=0; i<planet.info.length; i++){
+    let infotext=document.createElement("p");
+    infotext.innerText=` ${planet.info[i]}`;
+    infotext.style.fontSize="20px";
+    infotext.style.fontWeight="normal";
+    infotext.style.textAlign="justify";
+    newPlanet.appendChild(infotext);}
+    return newPlanet;
+}
+
+fetch (url)
+    .then(response =>response.json())
+    .then(data => createMarkUp(data,data))
+    .catch(error => console.log(error));
